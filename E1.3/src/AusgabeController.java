e1e2b32e76d4f48436276b0e3e946d176e4e1418
student <student>
2019-12-16 12:40:48 +0100

    1.1 noch hinzugefügt

--- E1.3/src/AusgabeController.java
+++ E1.3/src/AusgabeController.java
@@ -1,13 +1,18 @@
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.w3c.dom.Text;
 
 import java.awt.*;
 
 public class AusgabeController {
+    @FXML
+    private TextField Suchleiste, Textfeldgross;
+    @FXML
+    private Button Hinzufuegen, Reset;
+    @FXML
+    private void initialize(){
 
-    public Label helloWorld;
-
-    public void sayHelloWorld(ActionEvent actionEvent) {
-        helloWorld.setText("Hello World!");
     }
 }
