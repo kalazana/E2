2be150ee1e960c22c372b4ff232302e3e2bd5ad8
student <student>
2019-12-16 12:49:09 +0100

    1.1 noch hinzugefügt

--- E1.3/src/AusgabeController.java
+++ E1.3/src/AusgabeController.java
@@ -2,17 +2,18 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import org.w3c.dom.Text;
 
 import java.awt.*;
 
 public class AusgabeController {
     @FXML
-    private TextField Suchleiste, Textfeldgross;
+    private GridPane suchleiste, liste;
     @FXML
-    private Button Hinzufuegen, Reset;
+    private Button hinzufuegen, reset;
     @FXML
     private void initialize(){
-
+        hinzufuegen.setActionCommand((event)->{});
     }
 }
